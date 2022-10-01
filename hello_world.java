7b9bdf9665422a39d6b51e7e15c7474016ee6223
mariobob <35691890+Mariobob@users.noreply.github.com>
2022-10-01 08:20:20 -0400

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,3 +1,16 @@
 // This will print hello world in java
 
 System.out.println("Hello World");
+System.out.println("Hello World");
+System.out.println("Hello World");
+System.out.println("Hello13231 World");
+System.out.println("1 World");
+System.out.println("Hello23 World");
+System.out.println("Hello 2World");
+System.out.println("He13llo World");
+System.out.println("He32llo World");
+System.out.println("Hello W123rld112");
+System.out.println("Hello Wor2312ld");
+System.out.println("Hello 3123");
+System.out.println("Hello 312313World");
+System.out.println("Hello Worl1d");
